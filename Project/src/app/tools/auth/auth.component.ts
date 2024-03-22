2c9902dbc76ffd3cf63b440a433edd3a5c6c6a29
Vladislav Georgiev <vlafi90@gmail.com>
2024-03-22 12:23:25 +0200

    Added services for login, register and forgot password

--- Project/src/app/tools/auth/auth.component.ts
+++ Project/src/app/tools/auth/auth.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
 
 @Component({
   selector: 'app-auth',
@@ -7,8 +8,82 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
   state = AuthState.LOGIN;
+  firebasetsAuth: FirebaseTSAuth;
+  constructor() {
+    this.firebasetsAuth = new FirebaseTSAuth();
+  }
 
+  OnRegisterClick(
+    registerEmail: HTMLInputElement,
+    registerPassword: HTMLInputElement,
+    registerConfirmPassword: HTMLInputElement
+  ) {
+    let email = registerEmail.value;
+    let password = registerPassword.value;
+    let confirmPass = registerConfirmPassword.value;
 
+    if (
+      this.isNotEmpty(email) &&
+      this.isNotEmpty(password) &&
+      this.isNotEmpty(confirmPass) &&
+      this.isMatch(password, confirmPass)
+    ) {
+      this.firebasetsAuth.createAccountWith(
+        {
+          email: email,
+          password: password,
+          onComplete: (uc) => {
+            alert('Account created.');
+            registerEmail.value = "";
+            registerPassword.value = "";
+            registerConfirmPassword.value = "";
+          },
+          onFail: (err) => {
+            alert("Failed to create the account.");
+          }
+        }
+      );
+    }
+  }
+  OnLogin(
+    loginEmail: HTMLInputElement,
+    loginPassword: HTMLInputElement,
+  ) {
+    let email = loginEmail.value;
+    let password = loginPassword.value;
+
+    if (this.isNotEmpty(email) && this.isNotEmpty(password)) {
+      this.firebasetsAuth.signInWith({
+        email: email,
+        password: password,
+        onComplete: (uc) => {
+          alert("Logged in.")
+        },
+        onFail: (err) => {
+          alert(err);
+        }
+      });
+    }
+  }
+  OnResetClick(resetEmail: HTMLInputElement) {
+    let email = resetEmail.value;
+
+    if (this.isNotEmpty(email)) {
+      this.firebasetsAuth.sendPasswordResetEmail({
+        email: email,
+        onComplete: (uc) => {
+          alert(`Resent email sent to ${email}`)
+        }
+      });
+    }
+  }
+
+  isNotEmpty(text: string) {
+    return text != null && text.length > 0;
+  }
+  isMatch(text: string, comparedWith: string) {
+    return text == comparedWith;
+  }
 
   OnForgetPasswordClick() {
     this.state = AuthState.FORGOT_PASSWORD;
