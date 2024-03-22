b45a99954c4dda361872f153c102bcd012db2ad5
Vladislav Georgiev <vlafi90@gmail.com>
2024-03-22 10:53:00 +0200

    Initial commit

---
+++ Project/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebaseConfig: {
+        apiKey: "AIzaSyDDTAXx0P07GTUvvJEIvNFtnrGm9WvHgIM",
+        authDomain: "socialmediaproject-e372b.firebaseapp.com",
+        projectId: "socialmediaproject-e372b",
+        storageBucket: "socialmediaproject-e372b.appspot.com",
+        messagingSenderId: "361250895192",
+        appId: "1:361250895192:web:b30417e9ef2952a84e612d",
+        measurementId: "G-V6WQQWRSH6"
+    }
+};
